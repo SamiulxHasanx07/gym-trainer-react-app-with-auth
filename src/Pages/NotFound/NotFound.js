1077227e171d17f9c41fdbefb6e7065f1879c01a
Samiul Hasan <samiulxhasan650@gmail.com>
2022-04-18 20:22:03 +0600

    404 page done

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Not Found</h2>
-            <p>Please Try Again</p>
+        <div className='py-5' style={{minHeight:'100vh'}}>
+            <Container className='mt-5'>
+                <h2 className='sec-title text-center'>404 Not Found</h2>
+                <p className='text-center'>Please Try Again Valid Route or Visit Other Page</p>
+            </Container>
         </div>
     );
 };
